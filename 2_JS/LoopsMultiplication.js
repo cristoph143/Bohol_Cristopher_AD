a67fe03c5a35b9b10547fe7812821084e42813f3
Cristopher Bohol <cristopher.bohol.21@usjr.edu.ph>
2021-09-08 13:15:44 +0800

    Alignment VS2

--- 2_JS/LoopsMultiplication.js
+++ 2_JS/LoopsMultiplication.js
@@ -5,9 +5,14 @@ function loopsMul(base){
         for (let j = 1; j <= base; j++) {
             prod = i*j;
             newStr += prod;
-            if(prod < 10)
-                newStr += ' ';                                         
-            newStr += ' ';
+            if(prod > 99) 
+                newStr += '  ';
+            if(prod < 100)
+                newStr += '   ';
+                if(prod < 10)
+                    newStr += ' ';
+                newStr += ' ';
+            newStr += '';
         }
         newStr += "\n";
     }
