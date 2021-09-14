e2b43700a0824d7c08c1f965b51d9590ee7e2c9c
Cristopher Bohol <cristopher.bohol.21@usjr.edu.ph>
2021-09-14 17:52:15 +0800

    File Creation for Assignment

--- 3-nest/src/app.module.ts
+++ 3-nest/src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { Exercise3Module } from './exercise3/exercise3.module';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [Exercise3Module],
+  imports: [Exercise3Module, UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
