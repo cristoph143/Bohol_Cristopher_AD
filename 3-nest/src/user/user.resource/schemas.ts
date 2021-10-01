944ec396c373aa0aa6602db9c774b8b5c97c99f4
Cristopher Bohol <cristopher.bohol.21@usjr.edu.ph>
2021-10-01 14:56:29 +0800

    Fix??orNot??

---
+++ 3-nest/src/user/user.resource/schemas.ts
@@ -0,0 +1,33 @@
+//this is for documentation purposes only
+/*
+EXISTING USERS [name,age,email,password]
+['Leanne Graham',18,'sincere@april.biz','LG_123456']
+['Ervin Howell', 21, 'shanna@melissa.tv', 'EH_123123')]
+['Nathan Plains', 25, 'nathan@yesenia.net', 'NP_812415'],
+['Patricia Lebsack', 18, 'patty@kory.org', 'PL_12345']
+USER_REGISTER_BODY
+{
+	"name": string,
+	"age": number,
+	"email": string,
+	"password": string
+}
+USER_RETURN
+interface of CRUDReturn
+{
+    success: boolean,
+    data: //SPECIFIED IN ASSIGNMENT DOCUMENT
+}
+USER_DATA
+{
+      id: string
+      name: string,
+      age: number,
+      email: string
+}
+LOGIN_SCHEMA
+{
+    "email": string,
+    "password": string
+}
+*/
