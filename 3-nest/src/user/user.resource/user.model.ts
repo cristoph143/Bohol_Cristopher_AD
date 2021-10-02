a7f56a70aeac8e70095bf5d9e0aba2c047117e37
Cristopher Bohol <cristopher.bohol.21@usjr.edu.ph>
2021-10-02 14:51:28 +0800

    Refining API Submission Part 1 V2

---
+++ 3-nest/src/user/user.resource/user.model.ts
@@ -0,0 +1,45 @@
+import { CRUDReturn } from './crud_return.interface';
+import { Helper } from './helper';
+export class User {
+  public id: string;
+  private name: string;
+  private age: number;
+  private email: string;
+  private password: string;
+
+  constructor(name: string, age: number, email: string, password: string) {
+    this.id = Helper.generateUID();
+    this.name = name;
+    this.age = age;
+    this.email = email;
+    this.password = password;
+  }
+
+//   login(password: string): CRUDReturn {
+//     try {
+//       if (this.password === password) {
+//         return { success: true, data: this.toJson() };
+//       } else {
+//         throw new Error(`${this.email} login fail, password does not match`);
+//       }
+//     } catch (error) {
+//       return { success: false, data: error.message };
+//     }
+//   }
+
+//    matches(term: string): boolean {
+//    //hehe you didn't think I would actually give you the answers, yes?
+//   }
+
+//   replaceValues(body: any): boolean {
+//    //hehe you didn't think I would actually give you the answers, yes?
+//   }
+
+//   log() {
+//     console.log(this.toJson());
+//   }
+
+//   toJson() {    
+//    //hehe you didn't think I would actually give you the answers, yes?
+//   }
+}
