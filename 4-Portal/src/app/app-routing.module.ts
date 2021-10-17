1f1f3e5314b3acdf9812aa7f5f61c8a4d9342349
Cristopher Bohol <cristopher.bohol.21@usjr.edu.ph>
2021-10-17 16:31:15 +0800

    Into the Angular
    The Beginning

---
+++ 4-Portal/src/app/app-routing.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './screens/home/home.component';
+import { LoginComponent } from './screens/login/login.component';
+
+const routes: Routes = [
+  {
+    path: 'home',
+    component: HomeComponent
+  },
+  {
+    path: 'login',
+    component: LoginComponent
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
