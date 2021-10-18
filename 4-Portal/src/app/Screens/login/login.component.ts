5d44580fd53edfa860b41cac82d2efe4a09d5d55
Cristopher Bohol <cristopher.bohol.21@usjr.edu.ph>
2021-10-18 11:22:06 +0800

    ???

--- 4-Portal/src/app/Screens/login/login.component.ts
+++ 4-Portal/src/app/Screens/login/login.component.ts
@@ -13,3 +13,23 @@ export class LoginComponent implements OnInit {
   }
 
 }
+le = 'portal';
+  userCredential1:string = 'null';
+  userCredential2:string = 'null';
+
+  users: Array<any> = [
+    {username: 'potato@gmail.com', paswword: 'eyes'},
+    {username: 'potato1', paswword: 'eyes1'},
+    {username: 'potato2', paswword: 'eyes2'},
+    {username: 'potato3', paswword: 'eyes3'},
+    {username: 'potato4', paswword: 'eyes4'},
+    {username: 'potato5', paswword: 'eyes5'},
+    {username: 'potato6', paswword: 'eyes6'}
+  ];
+
+  login(email: string, password: string){
+    this.userCredential1 = email;
+    this.userCredential2 = password;
+  }
+
+}
