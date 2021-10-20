35fd302a0363b3e4d103b4055acfa9b91d075789
Cristopher Bohol <cristopher.bohol.21@usjr.edu.ph>
2021-10-20 20:49:22 +0800

    --❤Baseline❤--

--- 4-Portal/src/app/Screens/login/login.component.ts
+++ 4-Portal/src/app/Screens/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -7,29 +9,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
   }
 
-}
-le = 'portal';
-  userCredential1:string = 'null';
-  userCredential2:string = 'null';
-
-  users: Array<any> = [
-    {username: 'potato@gmail.com', paswword: 'eyes'},
-    {username: 'potato1', paswword: 'eyes1'},
-    {username: 'potato2', paswword: 'eyes2'},
-    {username: 'potato3', paswword: 'eyes3'},
-    {username: 'potato4', paswword: 'eyes4'},
-    {username: 'potato5', paswword: 'eyes5'},
-    {username: 'potato6', paswword: 'eyes6'}
-  ];
-
-  login(email: string, password: string){
-    this.userCredential1 = email;
-    this.userCredential2 = password;
+fCEmail = new FormControl();
+fCPassword = new FormControl();
+  login(){
+    //hello
   }
 
 }
