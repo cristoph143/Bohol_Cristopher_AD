91c3f704c5f01bd275b5084e45d3360b35db25e1
Cristopher Bohol <cristopher.bohol.21@usjr.edu.ph>
2021-10-21 20:31:11 +0800

    HOME+REG

--- 4-Portal/src/app/app-routing.module.ts
+++ 4-Portal/src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './screens/home/home.component';
 import { LoginComponent } from './screens/login/login.component';
+import { RegisterComponent } from './screens/register/register.component';
 
 const routes: Routes = [
   {
@@ -11,6 +12,10 @@ const routes: Routes = [
   {
     path: 'login',
     component: LoginComponent
+  },
+  {
+    path: 'register',
+    component: RegisterComponent
   }
 ];
 
--- 4-Portal/src/app/app.module.ts
+++ 4-Portal/src/app/app.module.ts
@@ -7,12 +7,14 @@ import { HomeComponent } from './screens/home/home.component';
 import { LoginComponent } from './screens/login/login.component';
 import { HttpClientModule } from '@angular/common/http';
 import { ReactiveFormsModule } from '@angular/forms';
+import { RegisterComponent } from './screens/register/register.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
     HomeComponent,
+    RegisterComponent,
     // RegisterComponent
   ],
   imports: [
