8c2984e271ea43eda7c28b8d5cb33a2f9be083db
Cristopher Bohol <cristopher.bohol.21@usjr.edu.ph>
2021-10-21 17:12:37 +0800

    LogIN Works????

--- 4-Portal/src/app/Screens/login/login.component.ts
+++ 4-Portal/src/app/Screens/login/login.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { FormControl } from '@angular/forms';
+import { FormControl, FormGroup } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
+import { Validators } from '@angular/forms';
+
 
 @Component({
   selector: 'app-login',
@@ -9,15 +13,35 @@ import { FormControl } from '@angular/forms';
 })
 export class LoginComponent implements OnInit {
 
-  constructor(private router: Router) { }
+  constructor(private router: Router, private api: HttpClient) { }
 
   ngOnInit(): void {
   }
 
-fCEmail = new FormControl();
-fCPassword = new FormControl();
-  login(){
-    //hello
-  }
+  logInForm: FormGroup = new FormGroup({
+    fCEmail: new FormControl('', Validators.required),
+    fCPassword: new FormControl('', Validators.required)
+  });
 
+  fcEmail = new FormControl();
+  fcPassword = new FormControl();
+  requestResult = '';
+  async login() {
+    var result: any = await this.api
+      .post(environment.API_URL + '/user/login', {
+        email: this.fcEmail.value,
+        password: this.fcPassword.value,
+      })
+      .toPromise();
+    console.log(result)
+    if (result.success == true) {
+      this.nav('home');
+    }
+    else {
+      alert(result.data);
+    }
+  }
+  nav(destination: string) {
+    this.router.navigate([destination]);
+  }
 }
