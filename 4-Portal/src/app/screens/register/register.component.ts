992b0d61f90e87a7114e352f38d0f4d8ed240f3d
Cristopher Bohol <cristopher.bohol.21@usjr.edu.ph>
2021-10-22 12:33:59 +0800

    RegCOntinuation

--- 4-Portal/src/app/screens/register/register.component.ts
+++ 4-Portal/src/app/screens/register/register.component.ts
@@ -1,4 +1,8 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-register',
@@ -7,9 +11,88 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router, private api: HttpClient) { }
 
   ngOnInit(): void {
+  }
+
+  registerForm: FormGroup = new FormGroup({
+    fcName: new FormControl('', Validators.required),
+    fcAge: new FormControl(0, [
+        Validators.required,
+        Validators.minLength(2), 
+        Validators.min(18), 
+        Validators.max(65)
+      ]),
+    fcEmail: new FormControl('', [
+        Validators.required,
+        Validators.email
+      ]),
+    fcPassword: new FormControl('', [
+        Validators.required,
+        Validators.pattern('^(?=.*[0-9])(?=.*[a-zA-Z])([a-zA-Z0-9]+)$')
+      ]),
+    fcPassword2: new FormControl('', Validators.required),
+  });
+
+  error: string = '';
+
+
+  async register() {
+    var result: any = await this.api
+      .post(environment.API_URL + '/user/register', {
+        name: this.registerForm.value.fcName,
+        age: this.registerForm.value.fcAge,
+        email: this.registerForm.value.fcEmail,
+        password: this.registerForm.value.fcPassword,
+      })
+      .toPromise();
+    console.log(result)
+    if (result.success == true) {
+      this.nav('login');
+    }
+    else {
+      alert(result.data);
+    }
+  }
+
+
+  onSubmit() {
+    alert('sdsds')
+    if (
+      this.registerForm.value['fcPassword'] !==
+      this.registerForm.value['fcPassword2']
+    ) {
+      this.error = 'Password doesnt match!';
+      return;
+    }
+    if (!this.registerForm.valid) {
+      {
+        this.error = 'No fields must be empty';
+        return;
+      }
+    }
+    if (this.registerForm.valid) {
+      var payload: {
+        name: string;
+        email: string;
+        age: number;
+        password: string;
+      };
+      payload = {
+        name: this.registerForm.value.fcName,
+        age: this.registerForm.value.fcAge,
+        email: this.registerForm.value.fcEmail,
+        password: this.registerForm.value.fcPassword,
+      };
+      console.log(payload);
+      alert('Hellow')
+      // this.register();
+    }
+  }
+
+  nav(destination: string) {
+    this.router.navigate([destination]);
   }
 
 }
