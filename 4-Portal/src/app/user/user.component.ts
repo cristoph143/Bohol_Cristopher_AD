5674a3cca5a342c81b826d69c8dc452c6de52eee
Cristopher Bohol <cristopher.bohol.21@usjr.edu.ph>
2021-10-30 22:23:17 +0800

    Background Part2

--- 4-Portal/src/app/user/user.component.ts
+++ 4-Portal/src/app/user/user.component.ts
@@ -1,4 +1,7 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-user',
@@ -7,9 +10,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router, private api: HttpClient) { }
 
   ngOnInit(): void {
+    this.displayUsers();
   }
 
+  users!: Array<any>;
+
+  async displayUsers() {
+    var result: any = await this.api
+      .get(environment.API_URL + '/user/all')
+      .toPromise();
+    console.log(result)
+    if (result.success == true) {
+      this.users = result.data;
+      console.log(result);
+    }
+    else {
+      alert(result.data);
+    }
+  }
+  // table = document.getElementById('table');
+
+  // for(var i = 1; i < table.rows.length; i++) {
+  //   table.rows[i].onclick = function () {
+  //     //rIndex = this.rowIndex;
+  //     document.getElementById("fname").value = this.cells[0].innerHTML;
+  //     document.getElementById("lname").value = this.cells[1].innerHTML;
+  //     document.getElementById("age").value = this.cells[2].innerHTML;
+  //   };
+  // }
 }
